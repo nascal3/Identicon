ebb8a235a6a9cbbd2a9de9e37e791b5485ea9ea6
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-13 16:28:49 +0300

    added the  Erlang's egd library to decencies

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,11 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
@@ -52,7 +57,7 @@ defmodule Identicon do
     %Identicon.Image{hex: hex} = image
     grid =
       hex
-      |> Enum.chunk(3)
+      |> Enum.chunk_every(3)
       |> Enum.map(&mirror_row/1)
       |>List.flatten
       |>Enum.with_index
