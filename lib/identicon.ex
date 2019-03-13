04e214bf3d41729840c76005e697c9be5903da90
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-13 13:40:01 +0300

    create color property from the hex and place it inside structure

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,20 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+     %Identicon.Image{hex: [red, blue, green | _tail]} = image
+
+     %Identicon.Image{image | color: {red, blue, green}}
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 
 end
